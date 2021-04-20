ccf1f6e533ce88731661fd3644affdfa95d37ecb
rynosk <73184813+rynosk@users.noreply.github.com>
2021-04-20 23:04:14 +0900

    Add files via upload
    やりたいこと
    画像１が表示されている
    画像２は右側（left:100vwの位置）に、
    	display:noneで配置されている
    任意の時間が経過すると、
    ・画像１は左側（left:-100vwの位置）に移動、移動後に削除、
    ・画像２は中央（left:0の位置）に移動、
    ・画像３が右側（left:100vwの位置）に配置。
    ※繰り返し処理で画像の数字が順に回っていく
    
    transformで一枚の背景画像がスライドインするのは出来る、けど、
    スクロールバーが表示されてしまう（一瞬画面幅が膨れる）のが問題。
    JSでうまく処理できないか模索中。
    .slider と.slider（番号）で表示を管理して、widthを表示される画像にだけ付与するとその画像だけ表示されるのでうまく使いたい。

---
+++ new_slider.js
@@ -0,0 +1,6 @@
+function new_slider() {
+  let i = 1
+  const sliderArea = document.getElementById('slider_area');
+  
+}
+new_slider()
---
+++ slider.js
@@ -0,0 +1,19 @@
+// function slider() {
+//   let i = 1
+//   const slider_area = document.getElementById('slider_area')
+//   setInterval(function slide_action() {
+//     i++
+//     if (i == 4) {
+//       i = 1
+//       slider_area.classList.add('slider1')
+//       slider_area.classList.remove('slider3')
+//       return
+//     }
+//     slider_area.classList.add('slider' + i)
+//     slider_area.classList.remove('slider' + (i-1))
+//     // console.log(i)
+//   }, 4000)
+// }
+
+// slider()
+
